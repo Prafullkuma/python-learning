1896871b8602ccbc95647bdf36e8b8a45040b2b9
Prafull <prafull.kumar.45@gmail.com>
2026-01-06 21:13:32 +0530

    how to write condtion in the pythone

---
+++ lesson01/4-meaning.py
@@ -0,0 +1,13 @@
+meaning = 45
+print("")
+if meaning > 10:
+    print("You are right")
+else: 
+    print("you are not")
+
+
+#  Ternary Operator ( 10 < 2 ? "Do this" : "else do this") in JS 
+
+print("yes your are right") if meaning < 10 else print("your are not")
+
+# other operator also there
